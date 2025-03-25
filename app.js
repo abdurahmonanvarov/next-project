1c57f3cbff1c5deafe666c9bf93dc8ff3a91a1db
abdurahmonanvarov <dousolo9@gmail.com>
2025-03-25 15:08:21 +0500

    aa

--- app.js
+++ app.js
@@ -1,29 +1,24 @@
+"use strict";
 // "use strict";
 // //1-masala
 // // Constraints 0<n,ar[i]<=1000  ar = [1, 2, 3, 4, 10, 11]  bolganda hammsini qoshasan va uni javobi 31 chiqish kerak
 // import { WriteStream, createWriteStream } from "fs";
 // process.stdin.resume();
 // process.stdin.setEncoding("utf-8");
-
 // let inputString: string = "";
 // let inputLines: string[] = [];
 // let currentLine: number = 0;
-
 // process.stdin.on("data", function (inputStdin: string): void {
 //   inputString += inputStdin;
 // });
-
 // process.stdin.on("end", function (): void {
 //   inputLines = inputString.split("\n");
 //   inputString = "";
-
 //   main();
 // });
-
 // function readLine(): string {
 //   return inputLines[currentLine++];
 // }
-
 // function simpleArraySum(ar: number[]): number {
 //   let sum = 0;
 //   for (let i = 0; i < ar.length; i++) {
@@ -31,135 +26,98 @@
 //   }
 //   return sum;
 // }
-
 // function main() {
 //   const ws: WriteStream = createWriteStream(process.env["OUTPUT_PATH"]);
-
 //   const arCount: number = parseInt(readLine().trim(), 10);
-
 //   const ar: number[] = readLine()
 //     .replace(/\s+$/g, "")
 //     .split(" ")
 //     .map((arTemp) => parseInt(arTemp, 10));
-
 //   const result: number = simpleArraySum(ar);
-
 //   ws.write(result + "\n");
-
 //   ws.end();
 // }
-
 // //2-masala
-
 // // Constraints  1<=n, m<=10  1<=a[i]<=100 1<=b[j]<=100  bunda inputga 2 3
 // // 2 4
 // // 16 32 96  manashu sonlar kiritiladi va menga sen manashu sonlar ichidan output 3 bolishi kerak
-
 // ("use strict");
-
 // import { WriteStream, createWriteStream } from "fs";
 // process.stdin.resume();
 // process.stdin.setEncoding("utf-8");
-
 // let inputString: string = "";
 // let inputLines: string[] = [];
 // let currentLine: number = 0;
-
 // process.stdin.on("data", function (inputStdin: string): void {
 //   inputString += inputStdin;
 // });
-
 // process.stdin.on("end", function (): void {
 //   inputLines = inputString.split("\n");
 //   inputString = "";
-
 //   main();
 // });
-
 // function readLine(): string {
 //   return inputLines[currentLine++];
 // }
-
 // function getTotalX(a: number[], b: number[]): number {
 //   let count = 0;
 //   const maxA = Math.max(...a);
 //   const minB = Math.min(...b);
-
 //   for (let x = maxA; x <= minB; x++) {
 //     const divisibleByA = a.every((num) => x % num === 0);
 //     const divisibleIntoB = b.every((num) => num % x === 0);
-
 //     if (divisibleByA && divisibleIntoB) {
 //       count++;
 //     }
 //   }
 //   return count;
 // }
-
 // //saitni savolini tagiga shunde qilib qoyyabdi
-
 // function main() {
 //   const ws: WriteStream = createWriteStream(process.env["OUTPUT_PATH"]);
-
 //   const firstMultipleInput: string[] = readLine()
 //     .replace(/\s+$/g, "")
 //     .split(" ");
-
 //   const n: number = parseInt(firstMultipleInput[0], 10);
 //   const m: number = parseInt(firstMultipleInput[1], 10);
-
 //   const arr: number[] = readLine()
 //     .replace(/\s+$/g, "")
 //     .split(" ")
 //     .map((arrTemp) => parseInt(arrTemp, 10));
-
 //   const brr: number[] = readLine()
 //     .replace(/\s+$/g, "")
 //     .split(" ")
 //     .map((brrTemp) => parseInt(brrTemp, 10));
-
 //   const total: number = getTotalX(arr, brr);
-
 //   ws.write(total + "\n");
-
 //   ws.end();
 // }
-
 // //3-masala
 // //Constraints 1<=n<=1000 0<=scores[i]<=10sakkizinchi darajasi inputga
 // //10 5 20 20 4 5 2 25 1 kiritiladi va outputga 2 4 chishi lozim
-
 // "use strict";
-
 // import { WriteStream, createWriteStream } from "fs";
 // process.stdin.resume();
 // process.stdin.setEncoding("utf-8");
-
 // let inputString: string = "";
 // let inputLines: string[] = [];
 // let currentLine: number = 0;
-
 // process.stdin.on("data", function (inputStdin: string): void {
 //   inputString += inputStdin;
 // });
-
 // process.stdin.on("end", function (): void {
 //   inputLines = inputString.split("\n");
 //   inputString = "";
-
 //   main();
 // });
-
 // function readLine(): string {
 //   return inputLines[currentLine++];
 // }
-
 // function breakingRecords(scores: number[]): number[] {
 //   let highest = scores[0];
 //   let lowest = scores[0];
 //   let highBreaks = 0;
 //   let lowBreaks = 0;
-
 //   for (let i = 1; i < scores.length; i++) {
 //     if (scores[i] > highest) {
 //       highest = scores[i];
@@ -169,25 +127,17 @@
 //       lowBreaks++;
 //     }
 //   }
-
 //   return [highBreaks, lowBreaks];
 // }
-
 // function main() {
 //   const ws: WriteStream = createWriteStream(process.env["OUTPUT_PATH"]);
-
 //   const n: number = parseInt(readLine().trim(), 10);
-
 //   const scores: number[] = readLine()
 //     .replace(/\s+$/g, "")
 //     .split(" ")
 //     .map((scoresTemp) => parseInt(scoresTemp, 10));
-
 //   const result: number[] = breakingRecords(scores);
-
 //   ws.write(result.join(" ") + "\n");
-
 //   ws.end();
 // }
-
 //4-masala
